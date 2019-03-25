0cd1bf9b53d0364eef9426b4d59c59987f446acd
Viswam <ramakichenanev@dnb.com>
2019-03-25 20:12:17 +0530

    Removed older json-data code

--- src/app/components/list-component/list-component.component.ts
+++ src/app/components/list-component/list-component.component.ts
@@ -3,11 +3,6 @@ import { User } from '../../models/User';
 import { UserService } from '../../services/user.service';
 import { MatTableDataSource } from '@angular/material';
 
-const ELEMENT_DATA: User[] = [
-  {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz'},
-  {id: 1, name: 'Ervin Howell', username: 'Antonette', email: 'Shanna@melissa.tv'},
-];
-
 @Component({
   selector: 'app-list-component',
   templateUrl: './list-component.component.html',
@@ -22,11 +17,7 @@ export class ListComponentComponent implements OnInit {
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    // this.userService.getUsers().subscribe(users => {
-    //   this.users = users;
-    //   this.dataSource = new MatTableDataSource(this.users)
-    // });
-
+   
     this.userService.getAllUsers().subscribe(data => {
       this.users = data.map(e => {
         return {
